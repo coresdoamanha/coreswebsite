4e0b3d9d5f0ec9c70ee9ea2bcda6682b8be34761
Thays Resende Achucarro <thaysresende24@gmail.com>
2020-08-27 11:15:32 -0400

    finalizado

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
 import './styles.css';
 
 function Contact(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    function handleSendData(e) {
+        e.preventDefault();
+        console.log({
+            name,
+            email,
+            phone,
+            subject,
+            message
+        })
+    }
+
     return (
         <div>
             <div className="contact">
                 <Header title="Entre em contato conosco" />
             </div>
-            
+            <div className="formulario">
+                <p>Tem alguma dúvida, sugestão ou deseja entrar em contato conosco? Preencha o formulário ou nos contate através desses contatos, estamos disponíveis para te atender!</p>
+
+                <form onSubmit={handleSendData}>
+                    <div className="rowform">
+                        <input placeholder="Nome" type="text" value={name} onChange={(e) => { setName(e.target.value) }} />
+                        <input placeholder="E-mail" type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    </div>
+                    <div className="rowform">
+                        <input placeholder="Telefone" type="text" value={phone} onChange={(e) => { setPhone(e.target.value) }} />
+                        <input placeholder="Assunto" type="text" value={subject} onChange={(e) => { setSubject(e.target.value) }} />
+                    </div>
+                    <div className="rowformmessage">
+                        <input placeholder="Escreva sua mensagem..." type="text" value={message} onChange={(e) => { setMessage(e.target.value) }} />
+                    </div>
+
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+            <div className="sede">
+                <div className="coluna">
+                    <h2>Nossa sede</h2>
+                    <p>Nossa sede fica na Comunidade do Totó, em Recife! Venha nos visitar, vamos te receber de braços abertos e com muito carinho!</p>
+                    <p>Localização</p>
+                    <p>Sede de Fundação Rua Ana Carneiro de Albuquerque, 94 - Totó Recife – PE – CEP: 50.791-460 Espaço Cultural Cores do Amanhã Av. Garota de Ipanema Box – 02/03 Totó - Jardim Planalto CEP: 50920-680 Recife – PE – Brasil</p>
+                    <img src={img1} alt="teste" />
+                </div>
+                <div className="coluna">
+                    <img src={img2} alt="teste" />
+                    <p>Contato e informações</p>
+                    <p>Movimento Social e Cultural Cores do Amanhã movimentoculturalcoresdoamanha@gmail.com (81) 3037.2338 CNPJ: 13.449.687/0001/99 | Inscrição Municipal: 476.864-7 Decreto de Lei de utilidade Pública Municipal: 18.621/19 Jouse Barata de Queiroz - Presidente Contatos: (81) 9 8876-3593 / (81) 9 9678-1217 e-mail: coresdoamanha2019@gmail.com Coordenação Geral Adelson Henrique | Erinaldo Maia | Fagner Cleiton Email: admcoresdoamanha19@gmail.com Coordenação Pedagógica Elton Ramos (Psicólogo) | Raquel Araújo (Pedagoga) Maria da Conceição - Pedagoga | Alberto Pires – Jurídico</p>
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     );
--- src/pages/Infos/index.js
+++ src/pages/Infos/index.js
@@ -82,7 +82,7 @@ function Infos(){
             </div>
             <div className="ultima">
                 <h3>Outras formas de ajudar</h3>
-                <div className="cardimage">
+                <div className="cardimage orange">
                     <div>
                         <img src={img1} alt="teste" />
                     </div>
@@ -92,44 +92,44 @@ function Infos(){
                         <Button title="Quero contratar" to="/" />
                     </div>
                 </div>
-                <div className="cardimage">
+                <div className="cardimage red">
+                    <div>
+                        <h2>Seja um parceiro fixo</h2>
+                        <p>Deseja ser um parceiro fixo do Cores do Amanhã? Essa atitude nos ajuda a manter nosso espaço e as atividades que nele acontece, como festas, oficinas, estrutura básica, compra de materiais e lanches para as crianças que fazem parte das atividades do Cores. Como forma de agradecimento, colocamos a marca da sua empresa na Homepage do nosso site! Entre em contato conosco, sua contribuição é muito importante para continuarmos a fazer diferença na vida das pessoas da nossa comunidade.</p>
+                        <Button title="Quero ser um parceiro" to="/" />
+                    </div>
                     <div>
                         <img src={img2} alt="teste" />
                     </div>
-                    <div>
-                        <h2>Contratando um de nossos artistas</h2>
-                        <p>Precisando renovar algum espaço? Dar mais vida e alegria á alguma parede? A a arte tem esse poder! Contrate um de nossos artistas! Além de ter uma arte exclusiva sua, você vai estar ajudando o Cores do Amanhã. Nossos artistas fazem graffitis, aerografia e painéis e também desenhos que podem virar lindos quadros para você poder presentear você mesmo ou alguém querido. Se interessou? Entre em contato conosco para solicitar o seu trabalho!</p>
-                        <Button title="Quero contratar" to="/" />
-                    </div>
                 </div>
-                <div className="cardimage">
+                <div className="cardimage green">
                     <div>
                         <img src={img3} alt="teste" />
                     </div>
                     <div>
-                        <h2>Contratando um de nossos artistas</h2>
-                        <p>Precisando renovar algum espaço? Dar mais vida e alegria á alguma parede? A a arte tem esse poder! Contrate um de nossos artistas! Além de ter uma arte exclusiva sua, você vai estar ajudando o Cores do Amanhã. Nossos artistas fazem graffitis, aerografia e painéis e também desenhos que podem virar lindos quadros para você poder presentear você mesmo ou alguém querido. Se interessou? Entre em contato conosco para solicitar o seu trabalho!</p>
-                        <Button title="Quero contratar" to="/" />
+                        <h2>Seja um voluntário</h2>
+                        <p>Você também pode nos ajudar sendo um de nossos voluntários! Você gosta de trabalhar com crianças, jovens e a comunidade em geral? Pode desenvolver uma atividade artística que seja voltada a cultura e cidadania? Aqui é o lugar certo! Toda ajuda é muito bem vinda e o trabalho voluntário é a base de nosso espaço, vamos juntos levar educação, lazer muita cultura para nossa comunidade!</p>
+                        <Button title="Quero ser voluntário" to="/" />
                     </div>
                 </div>
-                <div className="cardimage">
+                <div className="cardimage yellow">
+                    <div>
+                        <h2>Compre artesanato feito na comunidade</h2>
+                        <p>Você também pode apoiar o Cores comprando artesanato feito pelos artistas da nossa comunidade. Bolsas, telas, camisetas, agendas.</p>
+                        <Button title="Quero comprar" to="/" />
+                    </div>
                     <div>
                         <img src={img4} alt="teste" />
                     </div>
-                    <div>
-                        <h2>Contratando um de nossos artistas</h2>
-                        <p>Precisando renovar algum espaço? Dar mais vida e alegria á alguma parede? A a arte tem esse poder! Contrate um de nossos artistas! Além de ter uma arte exclusiva sua, você vai estar ajudando o Cores do Amanhã. Nossos artistas fazem graffitis, aerografia e painéis e também desenhos que podem virar lindos quadros para você poder presentear você mesmo ou alguém querido. Se interessou? Entre em contato conosco para solicitar o seu trabalho!</p>
-                        <Button title="Quero contratar" to="/" />
-                    </div>
                 </div>
-                <div className="cardimage">
+                <div className="cardimage orange">
                     <div>
                         <img src={img5} alt="teste" />
                     </div>
                     <div>
-                        <h2>Contratando um de nossos artistas</h2>
-                        <p>Precisando renovar algum espaço? Dar mais vida e alegria á alguma parede? A a arte tem esse poder! Contrate um de nossos artistas! Além de ter uma arte exclusiva sua, você vai estar ajudando o Cores do Amanhã. Nossos artistas fazem graffitis, aerografia e painéis e também desenhos que podem virar lindos quadros para você poder presentear você mesmo ou alguém querido. Se interessou? Entre em contato conosco para solicitar o seu trabalho!</p>
-                        <Button title="Quero contratar" to="/" />
+                        <h2>Caminho das Cores</h2>
+                        <p>Quer conhecer mais Recife, o Cores do Amanhã e nossa comunidade? Participe do Caminho das Cores! Através dessa iniciativa oferecemos visitas turísticas para conhecer os graffitis lindos da nossa região, o nosso bairro e o Cores!</p>
+                        <Button title="Quero participar" to="/" />
                     </div>
                 </div>
             </div>
